906eabea4cdc786841422c5967cc0432e262ba4e
kurnia123 <rizkykurnia219@gmail.com>
2021-08-15 11:21:05 +0700

    implemented authentication

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+const ClientError = require('./exceptions/ClientError');
+
 const music = require('./api/songs');
 const MusicService = require('./services/MusicService');
 const MusicsValidator = require('./validator/songs');
-const ClientError = require('./exceptions/ClientError');
+
+// users
+const users = require('./api/users');
+const UsersService = require('./services/UserService');
+const UsersValidator = require('./validator/users');
+
+const authentications = require('./api/authentication');
+const AuthenticationsService = require('./services/AuthenticationService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentication');
 
 const init = async () => {
     const musicService = new MusicService();
+    const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
+
     const server = Hapi.server({
         port: process.env.PORT,
         host: process.env.HOST,
@@ -18,13 +33,53 @@ const init = async () => {
         },
     });
 
-    await server.register({
-        plugin: music,
-        options: {
-            service: musicService,
-            validator: MusicsValidator,
+    await server.register([
+        {
+            plugin: Jwt,
         },
+    ]);
+
+    server.auth.strategy('musicsapp_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
+
+    await server.register([
+        {
+            plugin: music,
+            options: {
+                service: musicService,
+                validator: MusicsValidator,
+            },
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
+            },
+        },
+    ]);
 
     server.ext('onPreResponse', (request, h) => {
         const { response } = request;
